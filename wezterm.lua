eb6a2b5c9dfc8f2fd1b4c8d3aea7b15884843bf7
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-07-16 09:30:23 +0100

    initial config

---
+++ wezterm.lua
@@ -0,0 +1,17 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.color_scheme = "AdventureTime"
+config.font_size = 18.0
+config.font = wezterm.font("MesloLGS NF")
+
+config.keys = {
+	{ key = "{", mods = "SHIFT|ALT", action = act.MoveTabRelative(-1) },
+	{ key = "}", mods = "SHIFT|ALT", action = act.MoveTabRelative(1) },
+}
+
+return config
