c63d317a7734d65c9ee195b7f361b70646ce9edd
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-08-24 11:55:10 +0100

    font size

--- wezterm.lua
+++ wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 local act = wezterm.action
 
 config.font = wezterm.font("MesloLGS NF")
-config.font_size = 18.0
+config.font_size = 16.0
 config.send_composed_key_when_left_alt_is_pressed = true
 
 config.keys = {
